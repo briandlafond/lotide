19959ada3f45109bdde9042662f7252ac0be1578
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-01 21:41:43 +0000

    commit

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log('👌👌👌Assertion Passed: ' + actual + ' === ' + expected)
+    } else if (actual !== expected) {
+        console.log('😒😒😒Assertion Failed: ' + actual + ' !== ' + expected)
+    }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
