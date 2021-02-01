e59ff58eebd5aab2bf30f31972076d98990c4c71
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-01 23:01:54 +0000

    head commit

---
+++ head.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`👌👌👌Assertion Passed: ${actual} === ${expected}`)
+    } else if (actual !== expected) {
+        console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+function head(arr) {
+    return arr[0]
+}
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
