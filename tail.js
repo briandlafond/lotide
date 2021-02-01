3416b3f381e79b972a57e15a0648738ae27e212a
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-01 23:22:55 +0000

    tail commit

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`👌👌👌Assertion Passed: ${actual} === ${expected}`)
+    } else if (actual !== expected) {
+        console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+function tail(words) {
+    return words.slice(1)
+}
