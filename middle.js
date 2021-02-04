cd1a71e42b9049dc2d84448acfec03ea6aab8f75
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-04 20:31:16 +0000

    another commit

--- middle.js
+++ middle.js
@@ -33,11 +33,3 @@ const middle = function(arr) {
         }
     }
 }
-
-
-assertArraysEqual(middle([1])) // => []
-assertArraysEqual(middle([1, 2])) // => []
-assertArraysEqual(middle([1, 2, 3])) // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5])) // => [3]
-assertArraysEqual(middle([1, 2, 3, 4])) // => [2, 3] 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
