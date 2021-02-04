045db622fb5553e80ddc07685dad0d55c4ad36e0
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-04 22:40:46 +0000

    takeUntil complete

---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+const assertArraysEqual = function(actual, expected) {
+
+    let isEqual = eqArrays(actual, expected);
+
+    if (isEqual) {
+        console.log(`👌👌👌Assertion Passed: ${actual} === ${expected}`)
+    } else {
+        console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`)
+    }
+};
+
+function eqArrays(arr1, arr2) {
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true
+}
+
+const takeUntil = function(array, callback) {
+    let slicedArray = [];
+    for (const element of array) {
+        if (callback(element)) {
+            return slicedArray;
+        } else {
+            slicedArray.push(element);
+        }
+
+    }
+    return slicedArray;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
+
+
+// expected output
+//
+// [1, 2, 5, 7, 2]
+// --- 
+//['I\'ve', 'been', 'to', 'Hollywood']
