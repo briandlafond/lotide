5dd696a19403c47169ebdb766abb3f59ec80536a
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-08 18:03:13 +0000

    test file addition

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 5]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+module.exports = eqArrays;
+module.exports = assertArraysEqual;
