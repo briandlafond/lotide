87be13b0b72fe823998331b331f750c9b4b06632
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-08 20:08:44 +0000

    mocha test addition

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,15 @@
 const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle')
+
+describe("#middle", () => {
+    it("returns [2] for [1, 2, 3]", () => {
+        assert.deepEqual(middle([1, 2, 3]), [2]);
+    });
+    it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+        assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+    });
+});
 
 console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
