0a299c53a900aba743cb66275e9946fe3b2c0aba
Brian LaFond <brian.d.lafond@gmail.com>
2021-02-08 17:34:43 +0000

    test refactor

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`👌👌👌Assertion Passed: ${actual} === ${expected}`)
-    } else if (actual !== expected) {
-        console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`)
-    }
-};
+const assertEqual = require('./assertEqual'); //imports the assertEqual function into this file
 
 const head = function(array) {
     if (array.length > 0) {
@@ -14,5 +8,4 @@ const head = function(array) {
     }
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
